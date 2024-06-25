82eec47cb0a7dafa251ac8a744a4c14ff8ab96b2
KPetrosyants <kostya.petrosyants@bk.ru>
2024-06-25 14:20:06 +1000

    added accordeon in pricing

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -4,7 +4,9 @@ import '../assets/scss/pricing.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { usePriceSwitcher } from './components/pricing/priceSwticher.js';
+import { UseAccord } from './components/accord.js';
 
 useTheme();
 useBurger();
 usePriceSwitcher();
+UseAccord();
