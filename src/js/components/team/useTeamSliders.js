c962e354f75c8b4b316691a3e15a8b27ae4d1668
KPetrosyants <kostya.petrosyants@bk.ru>
2024-06-30 17:26:07 +1000

    finish team page

--- src/js/components/team/useTeamSliders.js
+++ src/js/components/team/useTeamSliders.js
@@ -1,6 +1,7 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 import { Navigation } from 'swiper/modules';
+
 export const useTeamSliders = () => {
   new Swiper('.team-slider', {
     modules: [Navigation],
@@ -11,6 +12,18 @@ export const useTeamSliders = () => {
 
     navigation: {
       nextEl: '.team-slider__button-next',
+    },
+  });
+
+  new Swiper('.teammate__slider', {
+    modules: [Navigation],
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 32,
+    slidesPerView: 'auto',
+
+    navigation: {
+      nextEl: '.teammate__slider-buttont-next',
     },
   });
 };
