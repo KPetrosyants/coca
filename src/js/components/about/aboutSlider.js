0551172a6978c00954b3fe7ca0a05e29ceeba8e9
KPetrosyants <kostya.petrosyants@bk.ru>
2024-07-07 19:26:30 +1000

    Finish about page

--- src/js/components/about/aboutSlider.js
+++ src/js/components/about/aboutSlider.js
@@ -7,9 +7,42 @@ export const useAboutSlider = () => {
     modules: [Navigation],
     slidesPerView: 'auto',
     spaceBetween: 32,
+    centeredSlides: false,
     loop: true,
     navigation: {
       nextEl: '.about__slider-button-next',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      740: {
+        centeredSlides: false,
+        slidesPerView: 'auto',
+      },
+    },
+  });
+  new Swiper('.articles__slider', {
+    modules: [Navigation],
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    loop: false,
+    navigation: {
+      prevEl: '.articles__slider-button-prev',
+      nextEl: '.articles__slider-button-next',
+    },
+    breakpoints: {
+      400: {
+        slidesPerView: 1.5,
+        centeredSlides: true,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+      880: {
+        centeredSlides: false,
+        slidesPerView: 3,
+      },
     },
   });
 };
